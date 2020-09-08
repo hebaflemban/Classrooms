e48600c70d5237b0088507cb35cb4ff7465df242
Raghad Alrasheed <raghad@MacBook-Pro-alkhas-b-Raghad.local>
2020-09-08 09:54:06 +0300

    signup signin signout fixed

--- classes/views.py
+++ classes/views.py
@@ -1,8 +1,52 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
 from .models import Classroom
 from .forms import ClassroomForm
+from django.contrib.auth import login, authenticate, logout
+
+def signup(request):
+    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            user.set_password(user.password)
+            user.save()
+
+            login(request, user)
+            return redirect("classroom-list")
+    context = {
+        "form":form,
+    }
+    return render(request, 'signup.html', context)
+
+def signin(request):
+    form = SigninForm()
+    if request.method == 'POST':
+        form = SigninForm(request.POST)
+        if form.is_valid():
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            auth_user = authenticate(username=username, password=password)
+            if auth_user is not None:
+                login(request, auth_user)
+                return redirect('classroom-list')
+    context = {
+        "form":form
+    }
+    return render(request, 'signin.html', context)
+def access(request):
+    context = {
+        "message":"you are not allowed"
+    }
+    return render(request, 'access.html', context)
+def signout(request):
+    logout(request)
+    return redirect("signin")
+
 
 def classroom_list(request):
 	classrooms = Classroom.objects.all()
